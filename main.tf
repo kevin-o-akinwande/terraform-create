dea7261dbc00d3d85df309cab17e21353d15b00b
Kevin Akinwande <kevin.o.akinwande@avanade.com>
2025-03-19 11:00:51 -0400

    added locals

--- main.tf
+++ main.tf
@@ -12,12 +12,23 @@
  provider "azurerm" {
    features {}
  }
-    
- # Create the very first resource
- resource "azurerm_resource_group" "contoso_rg" {
-   name     = "contoso_rg"
-   location = "eastus"
-   tags     = {
-   cost_center = "contoso research"
+
+ locals {
+    prefix = "contoso"
+    region = "eastus"
+    tags   = {
+      cost_center = "contoso research"  
+    }
  }
+    
+  resource "azurerm_resource_group" "contoso_rg" {
+   name         = "${local.prefix}_rg"
+   location     = local.region
+   tags         = local.tags
+ }
+
+ resource "azurerm_resource_group" "contoso_dev_rg" {
+    name        = "${local.prefix}_dev_rg"
+    location    = local.region
+    tags        = local.tags
  }
